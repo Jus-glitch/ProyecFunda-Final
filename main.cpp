0bc33ff65e39325971f0efd0e4034ca76c778882
ArturoL01 <arturlopezsoto34p@gmail.com>
2025-06-13 18:18:51 -0600

    lopez

---
+++ main.cpp
@@ -0,0 +1,14 @@
+int pedirNumeroEntero (const string& mensaje) {
+    int numero;
+    while (true) {
+    cout << mensaje;
+    cin >> numero;
+    if (cin.fail() || numero <= 0) { // Verificar si la entrada es válida
+    cin.clear () ; // Limpiar el estado de error
+    cin. ignore (numeric_limits<streamsize>: :max () , '\n') ; // Ignorar la entrada incorrecta
+    cout << "Entrada invalida. Intente de nuevo. \n";
+    else {
+    cin. ignore (numeric_limits<streamsize>: :max () , '\n') ; // Ignorar el resto de la linea
+    break; // Salir del bucle si la entrada es válida
+    
+    return numero;
